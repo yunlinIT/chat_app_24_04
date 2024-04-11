f468f9d0e808b2a14c3390d1a679ea0721a6d9e1
yunlinIT <yunlinit@gmail.com>
2024-04-11 15:30:06 +0900

    Post /chat/writeMessage

--- src/main/java/com/koreait/exam/chat_app_24_04/ChatApp2404Application.java
+++ src/main/java/com/koreait/exam/chat_app_24_04/ChatApp2404Application.java
@@ -11,3 +11,4 @@ public class ChatApp2404Application {
     }
 
 }
+
---
+++ src/main/java/com/koreait/exam/chat_app_24_04/ChatController.java
@@ -0,0 +1,18 @@
+package com.koreait.exam.chat_app_24_04;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/chat")
+public class ChatController {
+    @PostMapping("/writeMessage")
+    @ResponseBody
+    public String writeMessage() {
+        return "메세지 작성됨";
+    }
+
+}
