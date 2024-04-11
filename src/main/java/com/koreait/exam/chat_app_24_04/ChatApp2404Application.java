0c12c4e298a149ed0d466803ff9bf0b272d0da99
yunlinIT <yunlinit@gmail.com>
2024-04-11 14:51:10 +0900

    chatApp 프로젝트 세팅

---
+++ src/main/java/com/koreait/exam/chat_app_24_04/ChatApp2404Application.java
@@ -0,0 +1,13 @@
+package com.koreait.exam.chat_app_24_04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApp2404Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatApp2404Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/koreait/exam/chat_app_24_04/ChatApp2404ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koreait.exam.chat_app_24_04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatApp2404ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
